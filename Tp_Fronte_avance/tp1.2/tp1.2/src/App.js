2239f0a553ec73ccb08cf98f5f7ee6f52fd0a4bd
Jonas <jonasakenande7@gmail.com>
2025-12-16 15:35:44 +0100

    formulaire d'inscription (début)

--- Tp_Fronte_avance/tp1.2/tp1.2/src/App.js
+++ Tp_Fronte_avance/tp1.2/tp1.2/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Coucou je crois que Jonas a modifié l'Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coucou voilà un Formulaire d'inscription</h1>
+
     </div>
   );
 }

0b2ae6407a01122881f1165f42b522a9c9cf1f5a
Jonas <jonasakenande7@gmail.com>
2025-12-16 15:51:14 +0100

    fin du formulaire

--- Tp_Fronte_avance/tp1.2/tp1.2/src/App.js
+++ Tp_Fronte_avance/tp1.2/tp1.2/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,21 @@ function App() {
     <div className="App">
       <h1>Coucou voilà un Formulaire d'inscription</h1>
 
+      <form>
+        <label htmlFor="nom">Nom :</label><br />
+        <input type="text" id="nom" name="nom" required /><br /><br />
+
+        <label htmlFor="prenom">Prénom :</label><br />
+        <input type="text" id="prenom" name="prenom" required /><br /><br />
+
+        <label htmlFor="email">Email :</label><br />
+        <input type="email" id="email" name="email" required /><br /><br />
+
+        <label htmlFor="password">Mot de passe :</label><br />
+        <input type="password" id="password" name="password" required /><br /><br />
+
+        <input type="submit" value="S'inscrire" />
+      </form>
     </div>
   );
 }

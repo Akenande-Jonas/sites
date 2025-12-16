c92f0be4fb5d27ec51f90de46e4aa875c34f2051
Jonas <jonasakenande7@gmail.com>
2025-12-16 17:42:41 +0100

    erreur encore de reglementation

--- Tp_Fronte_avance/tp1.3/src/index.js
+++ Tp_Fronte_avance/tp1.3/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Composant from './composant/Composant1';
+import Composant1 from './composant/Composant1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
